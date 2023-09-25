782d6d8433dc7dada2e1b670d8654b2ad2602b62
Md Abdur Rahman Sifat <mdabdurrahmansifat@gmail.com>
2023-09-25 19:52:05 +0600

    Donation page data loaded form the storage and show to the page,condition applied for no data

--- src/Components/Donation/Donation.jsx
+++ src/Components/Donation/Donation.jsx
@@ -1,15 +1,40 @@
 // import { useLoaderData } from "react-router-dom";
 
+import { useEffect, useState } from "react";
+import DonationSingleCard from "./DonationSingleCard";
+
 
 const Donation = () => {
-    // const allCards = useLoaderData();
-    const allDonatedCard = localStorage.getItem('donation');
-    const allDonatedCardParse = JSON.parse(allDonatedCard);
-    console.log(allDonatedCardParse)
+    const [donationCard, setDonationCard] = useState([]);
+    const [noData, setNoData] = useState("");
+    
+    // const allDonatedCard = localStorage.getItem('donatedCard');
+
+    useEffect(()=>{
+        const donation = JSON.parse(localStorage.getItem('donation'));
+        if(donation){
+            setDonationCard(donation);
+        }
+        else
+        {
+            setNoData("No Data Found");
+        }
+    },[])
+    // const allDonatedCardParse = JSON.parse(allDonatedCard);
     return (
-        <div>
-            
-        </div>
+        
+            <div>
+            {
+                
+                noData ? <h1 className="text-6xl flex justify-center items-center mt-48">{noData}</h1> : 
+                <div className="grid grid-cols-2 gap-6 mx-32 mt-16">
+                {
+                donationCard.map(card => <DonationSingleCard key={card.id} SingleCard={card}></DonationSingleCard>)
+                }
+                </div>
+            }
+            </div>
+        
     );
 };
 
--- src/Components/Donation/DonationSingleCard.jsx
+++ src/Components/Donation/DonationSingleCard.jsx
@@ -1,10 +1,25 @@
+import PropTypes from 'prop-types'
 
-const DonationSingleCard = () => {
+const DonationSingleCard = ({SingleCard}) => {
+    const { title, image, bg_color, button_color, text_color, button_text,donation_amount } =SingleCard || {};
     return (
+        <div
+        style={{background: `${bg_color}`}} 
+        className='flex rounded-lg'>
         <div>
-            
+            <img className=' w-56 h-56' src={image} alt="" />
+        </div>
+        <div className='pl-5 pt-4'>
+            <button className='px-2 py-2 rounded-lg'  style={{background: `${button_color}`, color: `${text_color}`}}>{button_text}</button>
+            <h1 className=' text-2xl mt-2'>{title}</h1>
+            <p className='mt-2' style={{color: `${text_color}`}}>$ {donation_amount}.00</p>
+            <button style={{background: `${button_color}`}} className='btn text-white font-semibold text-lg mt-5'>View Details</button>
+        </div>
         </div>
     );
 };
 
+DonationSingleCard.propTypes = {
+    SingleCard: PropTypes.object.isRequired,
+}
 export default DonationSingleCard;
